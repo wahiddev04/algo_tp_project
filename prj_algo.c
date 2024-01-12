10e179f32f47acc49e73654a9930015625b465e6
wahiddev <wahiddelzgamer@gmail.com>
2024-01-12 11:04:48 +0100

    recentrer la liste chainé

--- prj_algo.c
+++ prj_algo.c
@@ -4,21 +4,22 @@ typedef struct Noeux {
     int info;
     struct Noeux *suivant;
 } Noeux;
-void dessiner_list_chaine(Noeux *tete) {
-    Noeux *temp=tete;
-    int index=0;
-    int startX=10;
-    int startY=10;
+void dessiner_list_chaine(Noeux *tete, int screenWidth, int screenHeight) {
+    Noeux *temp = tete;
+    int index = 0;
+    
+    // Position de départ au milieu de la fenêtre
+    int startX = screenWidth / 2 - 80;
+    int startY = screenHeight / 2 - 100;
 
-    while (temp!=NULL) {
-        
-        int boxWidth=120;
-        int boxHeight=40;
-        int textOffsetX=10;
-        int textOffsetY=10;
+    while (temp != NULL) {
+        int boxWidth = 120;
+        int boxHeight = 40;
+        int textOffsetX = 10;
+        int textOffsetY = 10;
 
-        DrawText(TextFormat("%d", temp->info),startX +textOffsetX,startY+textOffsetY+index * (boxHeight + 10),20,BLACK);
-        DrawRectangleLines(startX,startY+index * (boxHeight + 10),boxWidth,boxHeight,BLACK); 
+        DrawText(TextFormat("%d", temp->info), startX + textOffsetX, startY + textOffsetY + index * (boxHeight + 10), 20, BLACK);
+        DrawRectangleLines(startX, startY + index * (boxHeight + 10), boxWidth, boxHeight, BLACK);
 
         temp = temp->suivant;
         index++;
