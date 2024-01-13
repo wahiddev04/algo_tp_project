d4fa9a6fc74c6479e1d1d803ab07c909b6688978
wahiddev <wahiddelzgamer@gmail.com>
2024-01-13 22:50:23 +0100

    fusioner les deux branches wversion Rversion et le projet finale

--- prj_algo.c
+++ prj_algo.c
@@ -1,14 +1,35 @@
 #include "raylib.h"
 #include <stdlib.h>
+
+
 typedef struct Noeux {
     int info;
     struct Noeux *suivant;
 } Noeux;
+
+
+void add_element_list(Noeux** tete, int donné) {
+    Noeux *n_noeux = (Noeux*)malloc(sizeof(Noeux));
+    n_noeux->info = donné;
+    n_noeux->suivant = NULL;
+
+    if (*tete== NULL) {
+        *tete = n_noeux;
+    } else {
+        Noeux *temp = *tete;
+        while (temp->suivant != NULL) {
+            temp = temp->suivant;
+        }
+        temp->suivant = n_noeux;
+    }
+}
+
+
 void dessiner_list_chaine(Noeux *tete, int screenWidth, int screenHeight) {
     Noeux *temp = tete;
     int index = 0;
     
-    // Position de départ au milieu de la fenêtre
+    
     int startX = screenWidth / 2 - 80;
     int startY = screenHeight / 2 - 100;
 
@@ -25,9 +46,49 @@ void dessiner_list_chaine(Noeux *tete, int screenWidth, int screenHeight) {
         index++;
     }
 }
-int main(){
 
-const int screenWidth = 800;
+
+void Trie_a_bulle_list(Noeux *tete, int screenWidth, int screenHeight) {
+    int perm;
+    Noeux *ptr;
+    Noeux *last = NULL;
+
+    if (tete == NULL || tete->suivant == NULL) {
+        return;
+    }
+
+    do {
+        perm = 0;
+        ptr = tete;
+
+        while (ptr->suivant != last) {
+            // Dessiner le contenu après chaque échange
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                dessiner_list_chaine(tete, screenWidth, screenHeight);
+                EndDrawing();
+                // Attendre avant de continuer
+            double startTime = GetTime();
+            while (GetTime() - startTime < 2.0) { }
+
+            if (ptr->info > ptr->suivant->info) {
+                // Échanger les données des nœuds
+                int temp = ptr->info;
+                ptr->info = ptr->suivant->info;
+                ptr->suivant->info = temp;
+                perm = 1;
+            }
+
+           
+
+            ptr = ptr->suivant;
+        }
+        last = ptr;
+    } while (perm);
+}
+
+int main() {
+    const int screenWidth = 800;
     const int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "Liste chaînée avec interface graphique Raylib");
@@ -52,8 +113,8 @@ const int screenWidth = 800;
         EndDrawing();
     }
 
-    return 0;
+    return 0;}
 
 
-    return 0;
-}
+
+
