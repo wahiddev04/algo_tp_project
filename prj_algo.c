27c014496f2518bcf8b00a29f077509159a1fa8f
wahiddev <wahiddelzgamer@gmail.com>
2024-01-13 22:28:14 +0100

    ajouter le main et le prototype des fonction

--- prj_algo.c
+++ prj_algo.c
@@ -27,6 +27,32 @@ void dessiner_list_chaine(Noeux *tete, int screenWidth, int screenHeight) {
 }
 int main(){
 
+const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Liste chaînée avec interface graphique Raylib");
+
+    Noeux *tete = NULL;
+    add_element_list(&tete, 6);
+    add_element_list(&tete, 4);
+    add_element_list(&tete, 9);
+    add_element_list(&tete, 1);
+    add_element_list(&tete, 5);
+
+    while (!WindowShouldClose()) {
+        
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        Trie_a_bulle_list(tete, screenWidth, screenHeight);
+
+        dessiner_list_chaine(tete, screenWidth, screenHeight);
+
+        EndDrawing();
+    }
+
+    return 0;
 
 
     return 0;
